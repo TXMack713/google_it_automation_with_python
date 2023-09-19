4ad45e2901756fe66b585ff3d0d0a266a6f40ce9
TXMack713 <mackattack713@gmail.com>
2023-09-19 17:10:59 -0500

    Update

--- testing/rearrange_test.py
+++ testing/rearrange_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Google IT Automation with Python - Unit Tests, Writing Unit Tests in Python
+# Google IT Automation with Python - Unit Tests, Writing Unit Tests and Edge Cases in Python
 
 from rearrange import rearrange_name
 import unittest
@@ -12,6 +12,21 @@ class TestRearrange(unittest.TestCase):
         print("Testcase: " + testcase)
         print("Expected: " + expected)
         print("Actual: " + rearrange_name(testcase))
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_one_name(self):
+        testcase = "Voltaire"
+        expected = "Voltaire"
         self.assertEqual(rearrange_name(testcase), expected)
 
 unittest.main()
