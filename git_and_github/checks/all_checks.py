2efa2a37626fbb3fd4aa484657404017efba6759
TXMack713 <mackattack713@gmail.com>
2023-10-06 13:00:30 -0500

    Fixed the os.path.exists line in the all_checks.py file

--- git_and_github/checks/all_checks.py
+++ git_and_github/checks/all_checks.py
@@ -8,15 +8,15 @@ import sys
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
-    return os.path.exist("/run/reboot-required")
+    return os.path.exists("/run/reboot-required")
 
 
 def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
-print("Everything okay.")
-sys.exit(0)
+    print("Everything okay.")
+    sys.exit(0)
 
 
 main()
