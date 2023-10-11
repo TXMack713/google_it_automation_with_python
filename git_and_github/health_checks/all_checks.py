0e3634be6a27ed60f6704241d39cc91619f27b76
TXMack713 <mackattack713@gmail.com>
2023-10-11 13:17:44 -0500

    Added a simple network connectivity check

--- git_and_github/health_checks/all_checks.py
+++ git_and_github/health_checks/all_checks.py
@@ -5,6 +5,7 @@
 import os
 import shutil
 import sys
+import socket
 
 
 def check_reboot():
@@ -13,7 +14,7 @@ def check_reboot():
 
 
 def check_disk_full(disk, min_gb, min_percent):
-    """Returns True if there isn't enough disk space, false otherwise."""
+    """Returns True if there isn't enough disk space, False otherwise."""
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
     percent_free = 100 * du.free / du.total
@@ -25,12 +26,24 @@ def check_disk_full(disk, min_gb, min_percent):
 
 
 def check_root_full():
+    """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise."""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+
 
 def main():
     checks = [
         (check_reboot, "Pending Reboot"),
-        (check_root_full, "Root partition full")
+        (check_root_full, "Root partition full"),
+        (check_no_network, "No working network.")
     ]
     everything_ok = True
     for check, msg in checks:
