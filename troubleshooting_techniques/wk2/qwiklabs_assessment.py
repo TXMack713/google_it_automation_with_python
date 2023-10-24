2b8bbb4a9a0d2cbd0b10cbebdba5a8450f69da57
TXMack713 <mackattack713@gmail.com>
2023-10-24 16:36:36 -0500

    Continued working on the assessment

--- troubleshooting_techniques/wk2/qwiklabs_assessment.py
+++ troubleshooting_techniques/wk2/qwiklabs_assessment.py
@@ -5,6 +5,33 @@
 # Fix a slow system with Python
 
 import subprocess
-src = "/data/prod/"
-dest = "/data/prod_backup/"
-subprocess.call(["rsync", "-arq", src, dest])
+import multiprocessing
+import os
+# from concurrent import futures
+
+# src = "/data/prod/"
+# dest = "/data/prod_backup/"
+src = "/home/txmack713/Desktop/GitHub/it_automation_python/troubleshooting_techniques/wk2/"
+dest = "/home/txmack713/Desktop/GitHub/it_automation_python/troubleshooting_techniques/wk2/copy/"
+
+
+def copy_files(source):
+    subprocess.call(["rsync", "-arq", source, dest])
+
+
+core_count = multiprocessing.cpu_count()
+
+if __name__ == '__main__':
+    # src = "/home/txmack713/Desktop/GitHub/it_automation_python/troubleshooting_techniques/wk2"
+    # dest = "/home/txmack713/Desktop/GitHub/it_automation_python/troubleshooting_techniques/wk2/copy"
+
+    directory_list = next(os.walk(src))[1]
+    full_paths = [os.path.join(src, directory) for directory in directory_list]
+    # for root, dirs, files in os.walk(src):
+    #     directories = [os.path.join(src, dir) for dir in dirs]
+    with multiprocessing.Pool(processes=core_count) as pool:
+        pool.map(copy_files, full_paths)
+
+    pool.join()
+    #         directory_list.append(path)
+
