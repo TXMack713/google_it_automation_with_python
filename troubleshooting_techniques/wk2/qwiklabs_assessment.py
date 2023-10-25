cf10cf167ad93abaabc6605525eb2dc37286c225
TXMack713 <mackattack713@gmail.com>
2023-10-25 13:58:57 -0500

    Completed the Qwiklabs Assessment for wk2 of Troubleshooting and Debugging.

--- troubleshooting_techniques/wk2/qwiklabs_assessment.py
+++ troubleshooting_techniques/wk2/qwiklabs_assessment.py
@@ -11,21 +11,31 @@ import os
 src = "/data/prod/"
 dest = "/data/prod_backup/"
 
-if not os.path.exists(dest):
-    os.mkdir(dest)
+path = os.getcwd()
+
+path_arr = path.split("/")
+path_arr.pop()
+new_path = "/".join(path_arr)
+
+new_src = new_path + src
+new_dest = new_path + dest
+
+if not os.path.exists(new_dest):
+   os.mkdir(new_dest)
 
 
 def copy_files(source):
-    subprocess.call(["rsync", "-arq", source, dest])
+    print(source)
+    subprocess.call(["rsync", "-aq", source, new_dest])
 
 
 core_count = multiprocessing.cpu_count()
 
 if __name__ == '__main__':
     directories = []
-    for root, dirs, files in os.walk(src):
-        for file in files:
-            directories.append(os.path.join(root, file))
+    for root, dirs, files in os.walk(new_src, topdown=False):
+        for dir in dirs:
+            directories.append(os.path.join(root, dir))
     with multiprocessing.Pool(processes=core_count) as pool:
         pool.map(copy_files, directories)
 
