1b82c163b94421961b4d22ec9d74292e400d32c6
TXMack713 <mackattack713@gmail.com>
2023-11-15 13:45:02 -0600

    Additional updates

--- automating_real_world_tasks/mod1/image_repair.py
+++ automating_real_world_tasks/mod1/image_repair.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
-import PIL
 # Google IT Automation with Python
 # Automating Real-World Tasks with Python
 # Module 1 - Qwiklabs Assessment: Scale and convert images using PIL
 
+import PIL
 from PIL import Image
 import os
 import shutil
 
 
-os.chdir("images")
 orig_path = os.getcwd()
 print(orig_path)
+
+if os.path.exists(orig_path+"/images"):
+    os.chdir("images")
+
 os.chdir("..")
 print(os.getcwd())
 new_dir = "opt"
 try:
-    os.mkdir(new_dir)
-    os.chdir(new_dir)
+    if os.path.exists(orig_path+"/opt"):
+        os.chdir(new_dir)
+    else:
+        os.mkdir(new_dir)
+        os.chdir(new_dir)
 except:
     print("Directory already exists")
     os.chdir(new_dir)
@@ -25,8 +31,11 @@ except:
 new_folder = "icons"
 
 try:
-    os.mkdir(new_folder)
-    os.chdir(new_folder)
+    if os.path.exists(orig_path + "/icons"):
+        os.chdir(new_folder)
+    else:
+        os.mkdir(new_folder)
+        os.chdir(new_folder)
 except:
     print("Directory already exists")
     os.chdir(new_folder)
@@ -40,12 +49,12 @@ for root, directory, files in os.walk(orig_path):
         full_file = os.path.join(root, file)
         print("Full file: " + full_file)
         if os.path.isfile(full_file):
-            new_file = new_path + "/" + file + ".jpeg"
+            new_file = new_path + "/" + file
             try:
                 with Image.open(full_file) as im:
-                    im.convert("RGB").rotate(90).resize((128, 128)).save(new_file, "JPEG")
-                # new_location = new_path + "/" + file +
-                # shutil.copy(full_file, new_location)
+                    im.convert("RGB").rotate(90).resize((128, 128)).save(full_file, "JPEG")
+                new_location = new_path + "/" + file
+                shutil.move(full_file, new_location)
             except PIL.UnidentifiedImageError:
                 pass
 
