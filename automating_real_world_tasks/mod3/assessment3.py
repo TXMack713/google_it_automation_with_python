f8b5d3968a22206f8b6e4a8acf514ecf1149b4ab
Anthony Mack <mackattack713@gmail.com>
2023-12-04 15:57:23 -0600

    Starting the assessment for module 3

---
+++ automating_real_world_tasks/mod3/assessment3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Google IT Automation with Python
+# Automating Real-World Tasks with Python
+# Module 3 - Qwiklabs Assessment: Automatically Generate a PDF and sending it by E-mail
+
+import email
+import os
+import smtplib
+
+
+
