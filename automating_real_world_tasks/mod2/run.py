feb27ec288249cb3ff695451401ee7bf8219cf59
Anthony Mack <mackattack713@gmail.com>
2023-12-04 15:34:58 -0600

    Completed the module 2 assessment for automating tasks with Python

--- automating_real_world_tasks/mod2/run.py
+++ automating_real_world_tasks/mod2/run.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # Google IT Automation with Python
 # Automating Real-World Tasks with Python
-# Module 2 - Qwiklabs Assessment: Process Text Files with Python Dictionaries ad Upload to Running Web Service
+# Module 2 - Qwiklabs Assessment: Process Text Files with Python Dictionaries and Upload to Running Web Service
 
 import os
 import requests
+import json
 
 print(os.getcwd())
 os.chdir("feedback")
@@ -16,7 +17,8 @@ print("Review path: " + review_path)
 # Serialize the data and post it to the database
 
 review_list = []
-
+if not response.ok:
+    raise Exception("GET failed with status code {}".format(response.status_code))
 for root, directory, files in os.walk(review_path):
     for file in files:
         review_dict = {}
@@ -49,3 +51,49 @@ for root, directory, files in os.walk(review_path):
         print("\n")
 
 print(review_list)
+
+'''
+#!/usr/bin/env python3
+
+# Module 2 - Process Text Files with Python Dictionaries and Uploading to Running Web Services
+
+import os
+import requests
+import json
+
+
+file_list = os.listdir("/data/feedback")
+print(file_list)
+
+for file in file_list:
+    full_file = "/data/feedback/" + file
+    print("Full file path: " + full_file)
+    review_dict = {}
+    title = ""
+    username = ""
+    date = ""
+    feedback = ""
+
+    with open(full_file) as review:
+        title = review.readline().strip()
+        name = review.readline().strip()
+        date = review.readline().strip()
+        feedback_array = review.readlines()
+        for x in feedback_array:
+            feedback += x + " "
+        review_dict["title"] = title
+        review_dict["name"] = name
+        review_dict["date"] = date
+        review_dict["feedback"] = feedback.strip()
+        print("Review_dict:")
+        print(review_dict)
+        
+    response = requests.post("http://34.74.99.104/feedback/", data=review_dict)    
+    if response.ok:    
+        response.request.url    
+        continue    
+    else:    
+        raise Exception("POST failed with status code {}".format(response.status_code))    
+    # print(response)    
+    # review_list.append(review_dict)      
+'''
